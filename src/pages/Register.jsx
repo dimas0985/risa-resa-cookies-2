65a1e3546bad93152e7ffd46d9a7d9c330348d68
dimas0985 <ardhid667@gmail.com>
2026-07-16 17:43:01 +0700

    test vercel register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,6 +3,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import api from '../services/api';
 
 const Register = () => {
+  console.log("REGISTER VERSION BARU");
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
